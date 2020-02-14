f3663209d6ef669edd5685f10475145bed0e2608
williamjar <williaj@stud.ntnu.no>
2020-02-14 01:00:51 +0100

    a

--- src/components/content.js
+++ src/components/content.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ProductCard } from '../subComponents/ProductCard';
-import { Container, CardDeck, Modal, CardGroup, CardColumns, Card,Row, Col  } from 'react-bootstrap';
+import { Container, CardDeck, Modal, CardGroup, CardColumns, Image, Card,Row, Col  } from 'react-bootstrap';
 let products = require('../resources/products.json');
 
 export const Content = () => {
@@ -13,14 +13,17 @@ export const Content = () => {
                  <Row className="justify-content-center">
 
                     {products.map(product =>
-                        <ProductCard product={product} onOpen={handleShow}/>
+                        <ProductCard key={product.name} product={product} />
 
                     )}
 
                    </Row>
 
-                  
+                   <Row className="justify-content-center">
+                   <Image className="m-4" src="https://cdn.shopify.com/s/files/1/1347/2157/files/mekanisk-white_9415c58d-cb9d-48d7-885e-b5e1fec1df40_320x.png?v=1560371000" alt="Responsive image"/>
+                   </Row>
 
+        
                 </div>
         )
 }
--- src/subComponents/ProductCard.js
+++ src/subComponents/ProductCard.js
@@ -14,19 +14,18 @@ export const ProductCard = ({product}) => {
 
     return(
         
-        <Card style={{ width: '18rem' }}  className="m-4 text-white border-0" bg="dark">
+        <Card style={{ width: '20rem' }}  className="m-4 text-white border-0" bg="dark">
             <ProductView onClose={handleClose} product={product} show={onOpen}/>
 
             <Card.Header>
-                <Card.Img variant="top" fluid src={product.image} />
+                <Card.Img variant="top" fluid="true" src={product.image} />
                 <Card.Title>{product.name}</Card.Title>
             </Card.Header>
-            <Button class="rounded-0" onClick={handleShow}>See more</Button>
+            <Button className="rounded-0" onClick={handleShow}>Read more</Button>
         </Card>
 
         
 
-       
 
     )
 
@@ -38,15 +37,21 @@ export const ProductView = ({onClose, product, show}) => {
 
     return(
         <Modal 
-            class="border-0 shadow"
+            className="border-0 shadow"
             show={show}
-            centered
+            
+            onHide={onClose} 
+            animation={true}
         >
-        <Modal.Body class="text-white">
-            <img width="500" src={product.image}/>
+            
+        <Modal.Body className="text-white p-4">
+            <Image fluid="true" width="500" src={product.image}/>
             {product.description}
         </Modal.Body>
         <Button variant="danger" onClick={onClose}>Close</Button>
         </Modal>
     )
 }
+
+
+// big cards on the bottom so you can easily compare
